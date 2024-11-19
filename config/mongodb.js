88cfb4df2bd1b5ca6574692d12745671dd9d7258
Abdul Alim <info.bd.abdulalim@gmail.com>
2024-11-19 21:25:10 +0600

    first commit

---
+++ config/mongodb.js
@@ -0,0 +1,9 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+  mongoose.connection.on("connected", () => console.log("Databse Connected"));
+  // await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`);
+  await mongoose.connect(process.env.MONGODB_URI);
+};
+
+export default connectDB;
